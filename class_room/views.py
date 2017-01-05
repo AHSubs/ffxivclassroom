9b1b0177043001e46b94d0fd1d1eb463493299e4
Dark Artek <Dark Artek>
2017-01-05 11:39:31 +0100

    starting views

--- class_room/views.py
+++ class_room/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Base,Job
+from django.utils import timezone
 # Create your views here.
+
+def classlist(request):
+    baselist = Base.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
+    return render(request,'class_list/classlist.html',{'baselist':baselist})
+
+def joblist(request):
+    listjob = Job.objects.filter(pub_date__lte=timezone.now()).oder_by('pub_date')
+    return render(request,'class_list/joblist.html',{'listjob':listjob})
