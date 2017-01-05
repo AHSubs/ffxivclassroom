a7161093a24f14bc789f45d6e54be75ac59966a4
Dark Artek <Dark Artek>
2017-01-05 10:55:22 +0100

    Reverting

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -31,7 +31,6 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = (
-    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
